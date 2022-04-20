2df1962ca0d4d0dc46527788961738fe2c7cf005
RaphaelFelten <raphael.felten@gmail.com>
2022-04-20 13:58:37 +0200

    Fixed toolbar flickering and added logo (#1)
    * Fixed toolbar flickering and added logo
    
    * Added the icon and changed the path to be absolute
    
    * :lipstick: Add background to logo
    
    * :rewind: revert podfile
    
    Co-authored-by: Florent Cardoen <f.cardoen@me.com>

--- src/app/shared/directives/fade-header.directive.ts
+++ src/app/shared/directives/fade-header.directive.ts
@@ -7,7 +7,6 @@ import {DomController} from '@ionic/angular';
 export class FadeHeaderDirective {
 
     @Input() toolbar: any;
-    private toolbarHeight = 44;
 
     constructor(
         private readonly domCtrl: DomController
@@ -16,16 +15,35 @@ export class FadeHeaderDirective {
     }
 
     @HostListener('ionScroll', ['$event']) onContentScroll(event) {
+        // Defining the scroll distance at which the background should be fully opaque
+        const scrollDist = 255;
+
         let scrollTop: number = event.detail.scrollTop;
-        if (scrollTop >= 255) {
-            scrollTop = 255;
+        if (scrollTop >= scrollDist) {
+            scrollTop = scrollDist;
         }
-        const hexDist = scrollTop.toString(16);
+        //const hexDist = scrollTop.toString(16);
         this.domCtrl.write(() => {
             const color = getComputedStyle(this.toolbar.el).getPropertyValue('--ion-toolbar-background');
-            this.toolbar.el.style.setProperty('--background', `${color}${hexDist}`);
+            //Unfortunately, not all browsers/devices currently support hex color with transparancy
+            //So it's best to work with rgba instead of hexadecimal
+            //this.toolbar.el.style.setProperty('--background', `${color}${hexDist}`);
+            const [r, g, b] = this.hexToRgb(color);
+            const opacity = (scrollTop / scrollDist).toFixed(2);
+            const rgba = `rgba(${r},${g},${b},${opacity})`;
+            this.toolbar.el.style.setProperty('--background', rgba);
         });
     }
 
-
+    // Converts a hexadecimal color to rgb
+    // Found on: https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+    private hexToRgb(hex: string): number[] {
+        hex = hex.trim();
+        hex = hex.startsWith('#') ? hex.substring(1) : hex;
+        const bigint = parseInt(hex, 16);
+        const r = (bigint >> 16) & 255;
+        const g = (bigint >> 8) & 255;
+        const b = bigint & 255;
+        return [r, g, b];
+    }
 }
